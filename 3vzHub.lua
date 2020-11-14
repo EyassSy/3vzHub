07a3f7cec2f6f33a910a18ee09329931f6b4bb25
EyassSy <51035404+EyassSy@users.noreply.github.com>
2020-11-15 00:05:20 +0100

    Update 3vzHub.lua

--- 3vzHub.lua
+++ 3vzHub.lua
@@ -47,10 +47,10 @@ local function IJGDINK_fake_script() -- Detecting.LocalScript
 	local Detecting = script.Parent
 	
 	if game.PlaceId == 2619187362 then
-		wait(5)
+		wait(3)
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/EyassSy/3vzHub/main/SPFS%203vzHub.lua", true))()
 	elseif game.PlaceId == 286090429 then
-		wait(5)
+		wait(3)
 		loadstring(game:HttpGet("https://pastebin.com/raw/6Xmai9q7", true))()
 		Detecting.Visible = false
 	end
